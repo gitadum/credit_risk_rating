91574203b2f250b4694c572f7c975e4b7fa9f33b
Adrien Dumont <dev@adum.io>
2021-09-22 12:37:15 +0200

    changements dans l'assertion de l'unicité des id

--- modelling.py
+++ modelling.py
@@ -8,15 +8,13 @@ from sklearn.pipeline import make_pipeline
 from sklearn.ensemble import RandomForestClassifier
 from preprocessing import preprocessor, get_preprocessed_set_column_names
 
-train = pd.read_csv('../02_data/application_train.csv')
-test = pd.read_csv('../02_data/application_test.csv')
+#%%
+train = pd.read_csv('02_data/application_train.csv')
+test = pd.read_csv('02_data/application_test.csv')
 
-try:
-    assert len(train.SK_ID_CURR.unique()) == train.shape[0]
-    assert len(test.SK_ID_CURR.unique()) == test.shape[0]
-except AssertionError:
-    raise AssertionError('`SK_ID_CURR` is not unic!')
-
+id_error_msg = lambda x: '`SK_ID_CURR` is not unic for {} set!'.format(x)
+assert len(train.SK_ID_CURR.unique()) == train.shape[0], id_error_msg('train')
+assert len(test.SK_ID_CURR.unique()) == test.shape[0], id_error_msg('test')
 train.set_index('SK_ID_CURR', inplace=True)
 test.set_index('SK_ID_CURR', inplace=True)
 
@@ -24,7 +22,6 @@ X, y = train.iloc[:, 1:], train.iloc[:, 0]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2)
 # %%
-
 X_transformed = preprocessor.fit_transform(X)
 #test_preprocessed = preprocessor.transform(test)
 
@@ -33,9 +30,7 @@ print(X_transformed.shape)
 df = pd.DataFrame(X_transformed,
                   columns=get_preprocessed_set_column_names(preprocessor))
 # %%
-
-model = make_pipeline(preprocessor, RandomForestClassifier()
-)
+model = make_pipeline(preprocessor, RandomForestClassifier())
 model.fit(X_train, y_train)
 # %%
 model.get_params()
