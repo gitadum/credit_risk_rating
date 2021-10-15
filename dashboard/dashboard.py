f6526d14bd9f47166513c3b488e694b92b239318
Adrien Dumont <dev@adum.io>
2021-10-15 10:13:30 +0200

    donut chart qui affiche la probabilité de défaut colonnes 1er squelette du tableau de bord

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -3,6 +3,7 @@
 #%%
 import streamlit as st
 import requests
+import matplotlib.pyplot as plt
 
 def request_prediction(model_uri, customer_id):
     r = requests.post(url=model_uri, data={'sk_id_curr': customer_id})
@@ -13,7 +14,7 @@ def request_prediction(model_uri, customer_id):
     
     return r.json()
 
-API_URI = 'http://127.0.0.1:5000'
+API_URI = 'http://127.0.0.1:5000/predict'
 
 # %%
 
@@ -28,12 +29,39 @@ if predict_btn:
     model_prediction = request_prediction(API_URI, id_input)
     pred_final = model_prediction['predict_final']
     pred_final_fr = {'Favorable': 'Acceptable', 'Unfavorable': 'Critique'}
-    st.header('Modélisation du risque de crédit :')
-    st.write("Niveau de risque : **{}**".format(pred_final_fr[pred_final]))
-    st.write('**{}%** de chances d\'aboutir à un défaut de paiement.'.format(
-        round(model_prediction['predict_proba'],1) * 100.0
-    ))
+    pred_proba = model_prediction['predict_proba']
+    prediction, explanation, customer_infos = st.columns(3)
+    
+    with prediction:
+        st.header('Modélisation du risque de crédit :')
+        proba_graph = st.container()
+        pred_text = st.container()
+        with proba_graph:
+            st.write('probabilité de défaut sur le crédit demandé :')
+#            plt.draw()
+            pie_sizes =[pred_proba, 1 - pred_proba]
+            # Create a pieplot
+            plt.pie(pie_sizes, colors=['blue', 'grey'])
+            # add a circle at the center to transform it in a donut chart
+            my_circle=plt.Circle( (0,0), 0.7, color='white')
+            p=plt.gcf()
+            plt.text(0,0,'{:.1f}%'.format(pred_proba * 100.0),
+                        horizontalalignment='center',
+                        verticalalignment='center',
+                        fontsize=30)
+            p.gca().add_artist(my_circle)
+            st.pyplot(p)
+        with pred_text:
+            st.write("Niveau de risque : **{}**".format(pred_final_fr[pred_final]))
+#            st.write('**{:.1f}%** de chances d\'aboutir à un défaut de paiement.'\
+#                .format(pred_proba * 100.0))
 
-st.header('Simuler d\'autres conditions d\'emprunt')
+        st.header('Simuler d\'autres conditions d\'emprunt')
+        new_simul = st.button('Simuler')
+    
+    with explanation:
+        st.header('Facteurs de prédiction')
+#       st.pyplot(customer_explain['force'])
 
-new_simul = st.button('Simuler')
+    with customer_infos:
+        st.header('Informations sur le client')
