8b08534d42fe40021af4d8d8d40091a4bc87259a
Adrien Dumont <dev@adum.io>
2021-10-15 12:06:41 +0200

    les requêtes `/predict` renvoient des codes statut

--- api/api.py
+++ api/api.py
@@ -6,12 +6,14 @@ sys.path.append('..')
 import joblib
 import pandas as pd
 import numpy as np
+import shap
 from flask import Flask, jsonify, request, render_template
+from preprocessing import get_preprocessed_set_column_names as get_feat_names
 
 app = Flask(__name__)
 
-
 model = joblib.load('../HomeCredit_DefaultRisk.pkl')
+shap.initjs()
 
 def final_predict(modl, X, threshold=0.5):
     return np.array(modl.predict_proba(X)[:,1] > threshold, dtype=int)
@@ -23,15 +25,19 @@ ideal_threshold = 0.6672414786824148
 def display_main_page():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     client_db = pd.read_csv('../02_data/application_test.csv', index_col=0)
     id = int(request.form['sk_id_curr'])
-    client_vals = client_db.loc[id, :]
+    result = {}
+    try:
+        client_vals = client_db.loc[id, :]
+    except KeyError:
+        result['status_code'] = 404
+        return jsonify(result)
     X = pd.DataFrame(client_vals.values.reshape(1,-1),columns=client_db.columns)
     for k,v in client_db.dtypes.items():
         X[k] = X[k].astype(v)
-    result = {}
     result['id'] = id
     result['predict_proba'] = model.predict_proba(X)[:,1][0]
     prediction = final_predict(model, X, threshold=ideal_threshold)[0]
@@ -39,7 +45,35 @@ def predict():
         result['predict_final'] = 'Favorable'
     elif prediction == 1:
         result['predict_final'] = 'Unfavorable'
+    result['status_code'] = 200
     return jsonify(result)
+
+def explain_prediction(id):
+    client_db = pd.read_csv('../02_data/application_test.csv', index_col=0)
+    result = {}
+    try:
+        client_vals = client_db.loc[id, :]
+    except KeyError:
+        result['status_code'] = 404
+        return jsonify(result)
+    X = pd.DataFrame(client_vals.values.reshape(1,-1),columns=client_db.columns)
+    for k,v in client_db.dtypes.items():
+        X[k] = X[k].astype(v)
+    Xp = model['p'].transform(X)
+    feature_names = get_feat_names(model['p'])
+    explainer = shap.TreeExplainer(model['m'])
+    shap_values = explainer.shap_values(Xp)
+    result['id'] = id
+    result['force'] = shap.force_plot(explainer.expected_value[1],
+                                      shap_values[1][0,:], Xp[0,:],
+                                      feature_names=feature_names)
+    result['decision'] = shap.decision_plot(explainer.expected_value[1],
+                                            shap_values[1],
+                                            feature_names=feature_names)
+    result['status_code'] = 200
+    return result
+
+
 
 @app.route('/dashboard/')
 def dashboard():
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,67 +1,78 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-#%%
+
 import streamlit as st
 import requests
 import matplotlib.pyplot as plt
+from json.decoder import JSONDecodeError
 
 def request_prediction(model_uri, customer_id):
     r = requests.post(url=model_uri, data={'sk_id_curr': customer_id})
     
-    if r.status_code != 200:
-        raise Exception(
-            "Request failed with status {}, {}".format(r.status_code, r.text))
-    
-    return r.json()
+#    if r.status_code != 200:
+#        raise Exception(
+#            "Request failed with status {}, {}".format(r.status_code, r.text))
+
+    result = {}
+    try:
+        result = r.json()
+        if result['status_code'] != 404:
+            result['status_code'] = r.status_code
+    except JSONDecodeError:
+        result['status_code'] = r.status_code
+    return result
 
 API_URI = 'http://127.0.0.1:5000/predict'
 
-# %%
-
 st.title('PretADepenser - KYC Dashboard')
 
-st.header('Identité du client')
-
-id_input = st.text_input(label='Entrez un ID Client')
+st.header('Demande de prêt')
+id_input = st.text_input(label='Entrez un numéro de dossier')
 predict_btn = st.button('Visualiser')
 
 if predict_btn:
     model_prediction = request_prediction(API_URI, id_input)
-    pred_final = model_prediction['predict_final']
-    pred_final_fr = {'Favorable': 'Acceptable', 'Unfavorable': 'Critique'}
-    pred_proba = model_prediction['predict_proba']
-    prediction, explanation, customer_infos = st.columns(3)
-    
-    with prediction:
-        st.header('Modélisation du risque de crédit :')
-        proba_graph = st.container()
-        pred_text = st.container()
-        with proba_graph:
-            st.write('probabilité de défaut sur le crédit demandé :')
-#            plt.draw()
-            pie_sizes =[pred_proba, 1 - pred_proba]
-            # Create a pieplot
-            plt.pie(pie_sizes, colors=['blue', 'grey'])
-            # add a circle at the center to transform it in a donut chart
-            my_circle=plt.Circle( (0,0), 0.7, color='white')
-            p=plt.gcf()
-            plt.text(0,0,'{:.1f}%'.format(pred_proba * 100.0),
-                        horizontalalignment='center',
-                        verticalalignment='center',
-                        fontsize=30)
-            p.gca().add_artist(my_circle)
-            st.pyplot(p)
-        with pred_text:
-            st.write("Niveau de risque : **{}**".format(pred_final_fr[pred_final]))
-#            st.write('**{:.1f}%** de chances d\'aboutir à un défaut de paiement.'\
-#                .format(pred_proba * 100.0))
+#    st.write(model_prediction)
+    if model_prediction['status_code'] == 404:
+        st.write('La demande de prêt n\'est pas présente dans la base.')
+    elif model_prediction['status_code'] != 200:
+        st.write('Une erreur s\'est produite.')
+    else:
+        pred_final = model_prediction['predict_final']
+        pred_final_fr = {'Favorable': 'Acceptable', 'Unfavorable': 'Critique'}
+        pred_proba = model_prediction['predict_proba']
+        prediction, explanation, customer_infos = st.columns(3)
+        
+        with prediction:
+            st.header('Modélisation du risque de crédit :')
+            proba_graph = st.container()
+            pred_text = st.container()
+            with proba_graph:
+                st.write('probabilité de défaut sur le crédit demandé :')
+    #            plt.draw()
+                pie_sizes =[pred_proba, 1 - pred_proba]
+                # Create a pieplot
+                plt.pie(pie_sizes, colors=['blue', 'grey'])
+                # add a circle at the center to transform it in a donut chart
+                my_circle=plt.Circle( (0,0), 0.7, color='white')
+                p=plt.gcf()
+                plt.text(0,0,'{:.1f}%'.format(pred_proba * 100.0),
+                            horizontalalignment='center',
+                            verticalalignment='center',
+                            fontsize=30)
+                p.gca().add_artist(my_circle)
+                st.pyplot(p)
+            with pred_text:
+                st.write("Niveau de risque : **{}**".format(pred_final_fr[pred_final]))
+    #            st.write('**{:.1f}%** de chances d\'aboutir à un défaut de paiement.'\
+    #                .format(pred_proba * 100.0))
 
-        st.header('Simuler d\'autres conditions d\'emprunt')
-        new_simul = st.button('Simuler')
-    
-    with explanation:
-        st.header('Facteurs de prédiction')
-#       st.pyplot(customer_explain['force'])
+            st.header('Simuler d\'autres conditions d\'emprunt')
+            new_simul = st.button('Simuler')
+        
+        with explanation:
+            st.header('Facteurs de prédiction')
+    #       st.pyplot(customer_explain['force'])
 
-    with customer_infos:
-        st.header('Informations sur le client')
+        with customer_infos:
+            st.header('Informations sur l\'emprunteur')
