127ce525ad228246bef7c2eeb19434d54bab6826
Adrien Dumont <dev@adum.io>
2021-11-01 16:31:01 +0100

    refonte de l’api et du tableau de bord - suppression de la fonction explain prediction - remplacée par la fonction get_app_details qui renvoie     - les données pour toute la base des demandes de prêt     - les données pour le numéro de dossier demandé     - les explications shap pour chaque prédiction - les graphiques shap sont migrés dans la partie tableau de bord     - l’api ne renvoie maintenant que des données - gestion de l'initjs de shap - diverses autres corrections et amélioration

--- api/api.py
+++ api/api.py
@@ -3,19 +3,20 @@
 
 import sys
 sys.path.append('..')
+
 import joblib
 import pandas as pd
 import numpy as np
 import shap
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request
 from preprocessing import get_preprocessed_set_column_names as get_feat_names
 from preprocess_funcs import add_secondary_table_features
 from timer import timer
 
 app = Flask(__name__)
 
+
 model = joblib.load('../HomeCredit_DefaultRisk.2.pkl')
-shap.initjs()
 
 def final_predict(modl, X, threshold=0.5):
     return np.array(modl.predict_proba(X)[:,1] > threshold, dtype=int)
@@ -24,76 +25,57 @@ def final_predict(modl, X, threshold=0.5):
 #ideal_threshold = 0.6672414786824148
 ideal_threshold = 0.6757005832464233
 
-@app.route('/', methods=['GET'])
-def display_main_page():
-    return render_template('index.html')
+#@app.route('/app_details', methods=['POST'])
+@timer
+def get_app_details(id):
+    app_db = pd.read_csv('../02_data/application_test.csv', index_col=0)
+    result = {}
+    try:
+        client_vals = app_db.loc[id, :]
+    except KeyError:
+        result['status_code'] = 404
+        return result
+    X = pd.DataFrame(client_vals.values.reshape(1,-1),columns=app_db.columns)
+    for k,v in app_db.dtypes.items():
+        X[k] = X[k].astype(v)
+    X = add_secondary_table_features(X)
+    result['id'] = id
+    result['app_db'] = app_db
+    Xp = model['p'].transform(X)
+    result['X'] = X
+    result['Xp'] = Xp
+    result['feature_names'] = get_feat_names(model['p'])
+    explainer = shap.TreeExplainer(model['m'])
+    shap_values = explainer.shap_values(Xp)
+    result['shap_explainer'] = explainer
+    result['shap_values'] = shap_values
+    result['status_code'] = 200
+    return result
 
 @app.route('/predict', methods=['POST'])
 @timer
 def predict():
-    client_db = pd.read_csv('../02_data/application_test.csv', index_col=0)
     id = int(request.form['sk_id_curr'])
+    d = get_app_details(id)
     result = {}
     try:
-        client_vals = client_db.loc[id, :]
-    except KeyError:
-        result['status_code'] = 404
-        return jsonify(result)
-    X = pd.DataFrame(client_vals.values.reshape(1,-1),columns=client_db.columns)
-    for k,v in client_db.dtypes.items():
-        X[k] = X[k].astype(v)
-    X = add_secondary_table_features(X)
-    result['id'] = id
-    result['predict_proba'] = model.predict_proba(X)[:,1][0]
-    prediction = final_predict(model, X, threshold=ideal_threshold)[0]
-    if prediction == 0:
-        result['predict_final'] = 'Favorable'
-    elif prediction == 1:
-        result['predict_final'] = 'Unfavorable'
-    result['status_code'] = 200
-    return jsonify(result)
+        assert d['status_code'] == 200
+    except AssertionError:
+        result['status_code'] = d['status_code']
+        return result
+    result['id'] = d['id']
+    result['default_proba'] = model.predict_proba(d['X'])[:,1][0]
+    result['risk'] = int(final_predict(model, d['X'], ideal_threshold)[0])
 
-def explain_prediction(id):
-    client_db = pd.read_csv('../02_data/application_test.csv', index_col=0)
-    result = {}
-    try:
-        client_vals = client_db.loc[id, :]
-    except KeyError:
-        result['status_code'] = 404
-        return jsonify(result)
-    X = pd.DataFrame(client_vals.values.reshape(1,-1),columns=client_db.columns)
-    for k,v in client_db.dtypes.items():
-        X[k] = X[k].astype(v)
-    X = add_secondary_table_features(X)
-    Xp = model['p'].transform(X)
-    feature_names = get_feat_names(model['p'])
-    explainer = shap.TreeExplainer(model['m'])
-    shap_values = explainer.shap_values(Xp)
-    result['id'] = id
-    force_plot = shap.force_plot(explainer.expected_value[1],
-                                      shap_values[1][0,:], Xp[0,:],
-                                      feature_names=feature_names,)
-#                                      matplotlib=True)
-#    decision_plot = shap.decision_plot(explainer.expected_value[1],
-#                                            shap_values[1],
-#                                            feature_names=feature_names)
-#    result['decision'] = decision_plot
-    result['force'] = force_plot
     for col in ['EXT_SOURCE_1','EXT_SOURCE_2', 'EXT_SOURCE_3',
                 'AMT_GOODS_PRICE', 'AMT_CREDIT']:
         result[col] = dict()
-        result[col]['value'] = client_db.loc[id, col]
-        result[col]['median'] = client_db[col].median()
+        result[col]['value'] = d['app_db'].loc[id, col]
+        result[col]['median'] = d['app_db'][col].median()
         result[col]['gap'] = result[col]['value'] - result[col]['median']
         result[col]['gap_pct'] = result[col]['gap'] / result[col]['median']
     result['status_code'] = 200
-    return result
-
-
-
-@app.route('/dashboard/')
-def dashboard():
-    render_template('dashboard.html')
+    return jsonify(result)
 
 
 if __name__ == '__main__':
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,20 +7,14 @@ sys.path.append('..')
 import streamlit as st
 import streamlit.components.v1 as components
 import shap
-import numpy as np
 import requests
 import matplotlib.pyplot as plt
 from json.decoder import JSONDecodeError
-from api.api import explain_prediction
+from api.api import get_app_details
 
-
+@st.cache
 def request_prediction(model_uri, customer_id):
     r = requests.post(url=model_uri, data={'sk_id_curr': customer_id})
-    
-#    if r.status_code != 200:
-#        raise Exception(
-#            "Request failed with status {}, {}".format(r.status_code, r.text))
-
     result = {}
     try:
         result = r.json()
@@ -36,10 +30,23 @@ API_URI = 'http://127.0.0.1:5000/predict'
 # Use the full page instead of a narrow central column
 st.set_page_config(layout="wide")
 
+#shap.initjs()
+
 def st_shap(plot, height=None):
     shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
     components.html(shap_html, height=height)
 
+def shap_force_plot(explainer, shap_values, Xp, feature_names):
+    return shap.force_plot(explainer.expected_value[1],
+                           shap_values[1][0,:], Xp[0,:],
+                           feature_names=feature_names)
+
+def shap_decision_plot(explainer, shap_values, feature_names):
+    return shap.decision_plot(explainer.expected_value[1],
+                              shap_values[1],
+                              feature_names=feature_names)
+
+# ## DÉBUT DU TABLEAU DE BORD ##
 st.title('PretADepenser - KYC Dashboard')
 
 st.header('Demande de prêt')
@@ -47,30 +54,41 @@ id_input = st.text_input(label='Entrez un numéro de dossier')
 predict_btn = st.button('Visualiser')
 
 if predict_btn:
-    model_prediction = request_prediction(API_URI, id_input)
-#    st.write(model_prediction)
-    if model_prediction['status_code'] == 404:
+    api_req = request_prediction(API_URI, id_input)
+#    st.write(api_req) # debug
+    if api_req['status_code'] == 404:
         st.write('La demande de prêt n\'est pas présente dans la base.')
-    elif model_prediction['status_code'] != 200:
+    elif api_req['status_code'] != 200:
         st.write('Une erreur s\'est produite.')
     else:
-        expl = explain_prediction(int(id_input))
-        pred_final = model_prediction['predict_final']
-        pred_final_fr = {'Favorable': 'Acceptable', 'Unfavorable': 'Critique'}
-        pred_proba = model_prediction['predict_proba']
+        details = get_app_details(int(id_input))
+        pred_final = api_req['risk']
+        pred_label = {0: 'Acceptable', 1: 'Critique'}
+        pred_proba = api_req['default_proba']
         colormap = plt.get_cmap('RdBu_r')
         color = colormap(pred_proba)
-        prediction, explanation = st.columns([1,2])
+
+        insights = st.container()
+        explain = st.container()
         
-        with prediction:
-            st.header('Risque de crédit', anchor='credit-risk')
-            proba_graph = st.container()
-            pred_text = st.container()
-            with proba_graph:
-                st.write('probabilité de défaut sur le crédit demandé :')
-    #            plt.draw()
+        with insights:
+            st.subheader('Demande de prêt N°{}'.format(api_req['id']),
+                         anchor='application')
+
+            main_infos, prediction = st.columns([2,1])
+
+            with main_infos:
+                st.write('Coucou!')
+        
+            with prediction:
+                st.subheader('Risque de crédit', anchor='credit-risk')
+#                proba_graph = st.container()
+#                pred_text = st.container()
+#                with proba_graph:
+                st.write('Probabilité de défaut sur le crédit demandé :')
                 pie_sizes =[pred_proba, 1 - pred_proba]
                 # Create a pieplot
+                plt.figure(figsize=(6,6))
                 plt.pie(pie_sizes, colors=[color, 'grey'])
                 # add a circle at the center to transform it in a donut chart
                 my_circle=plt.Circle( (0,0), 0.7, color='white')
@@ -81,35 +99,31 @@ if predict_btn:
                             fontsize=30)
                 p.gca().add_artist(my_circle)
                 st.pyplot(p)
-            with pred_text:
-                st.write("Niveau de risque : **{}**".format(pred_final_fr[pred_final]))
-    #            st.write('**{:.1f}%** de chances d\'aboutir à un défaut de paiement.'\
-    #                .format(pred_proba * 100.0))
+#                with pred_text:
+                st.write("Niveau de risque : **{}**".format(
+                    pred_label[pred_final]))
 
-            st.header('Simuler d\'autres conditions d\'emprunt')
-            new_simul = st.button('Simuler')
+                st.subheader('Simuler d\'autres conditions d\'emprunt')
+                new_simul = st.button('Simuler')
         
-        with explanation:
-            st.header('Facteurs de prédiction', anchor='predict-factors')
-            st_shap(expl['force'])
-#            st.write(expl)
+        with explain:
+            st.subheader('Facteurs de prédiction', anchor='predict-factors')
+            st_shap(shap_force_plot(details['shap_explainer'],
+                                    details['shap_values'],
+                                    details['Xp'],
+                                    details['feature_names']))
             for col in ['EXT_SOURCE_1','EXT_SOURCE_2', 'EXT_SOURCE_3',
                         'AMT_GOODS_PRICE', 'AMT_CREDIT']:
-                if expl[col]['value'] != np.nan:
-                    statmnt = '**{}**: {:.0f}%'.format(col,
-                                                       abs(expl[col]['gap_pct']\
-                                                           * 100.0))
-                    if expl[col]['gap_pct'] >= 0.0:
+                if str(api_req[col]['value']) != "nan":
+                    statmnt = '**{}**: {:.0f}%'.format(
+                        col, abs(api_req[col]['gap_pct'] * 100.0))
+                    if api_req[col]['gap_pct'] >= 0.0:
                         following = 'plus élevé que la médiane des demandes.'
                     else:
                         following = 'moins élevé que la médiane des demandes.'
                     st.write(statmnt + ' ' + following)
                 else:
-                    st.write('{}: valeur inconnue.'.format(col))
-#            st.write(type(expl['EXT_SOURCE_1']['value']))
-#            st.write(type(expl['EXT_SOURCE_2']['value']))
-    #            gap_extsource1 = expl['EXT_SOURCE_1'] - expl['EXT_SOURCE_1_median']
-    #            st.write(gap_extsource1)
+                    st.write('**{}**: valeur inconnue.'.format(col))
 
 #        with customer_infos:
 #            st.header('Informations sur l\'emprunteur')
