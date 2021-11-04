d07b9c5077ae003216147e881cf1e5f2f0273b33
Adrien Dumont <dev@adum.io>
2021-11-04 17:27:19 +0100

    force plot shap redimensionné réarangement du tableau de bord ajout de d'une barre de couleur similaire au style shap

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -9,6 +9,7 @@ import streamlit.components.v1 as components
 import shap
 import requests
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 from json.decoder import JSONDecodeError
 from api.api import get_app_details
 
@@ -31,15 +32,16 @@ API_URI = 'http://127.0.0.1:5000/predict'
 st.set_page_config(layout="wide")
 
 #shap.initjs()
-
-def st_shap(plot, height=None):
-    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
-    components.html(shap_html, height=height)
+def st_shap(plot, height=185, width=None):
+    shap_html = f"<head>{shap.getjs()}</head><body style=\"width: 3000px;\">\
+                  {plot.html()}</body>"
+    components.html(shap_html, height=height, width=width, scrolling=True)
 
 def shap_force_plot(explainer, shap_values, Xp, feature_names):
     return shap.force_plot(explainer.expected_value[1],
                            shap_values[1][0,:], Xp[0,:],
-                           feature_names=feature_names)
+                           feature_names=feature_names,
+                           contribution_threshold=0.05)
 
 def shap_decision_plot(explainer, shap_values, feature_names):
     return shap.decision_plot(explainer.expected_value[1],
@@ -47,9 +49,8 @@ def shap_decision_plot(explainer, shap_values, feature_names):
                               feature_names=feature_names)
 
 # ## DÉBUT DU TABLEAU DE BORD ##
-st.title('PretADepenser - KYC Dashboard')
+st.title('PretADepenser - KYC Dashboard', anchor='/')
 
-st.header('Demande de prêt')
 id_input = st.text_input(label='Entrez un numéro de dossier')
 predict_btn = st.button('Visualiser')
 
@@ -65,65 +66,65 @@ if predict_btn:
         pred_final = api_req['risk']
         pred_label = {0: 'Acceptable', 1: 'Critique'}
         pred_proba = api_req['default_proba']
-        colormap = plt.get_cmap('RdBu_r')
-        color = colormap(pred_proba)
+        red =  '#FF0D57'
+        blue = '#1E88E5'
+        shap_cm = LinearSegmentedColormap.from_list("", [blue, 'violet', red])
+        color = shap_cm(pred_proba)
 
         insights = st.container()
         explain = st.container()
         
         with insights:
-            st.subheader('Demande de prêt N°{}'.format(api_req['id']),
+            st.header('Demande de prêt N°{}'.format(api_req['id']),
                          anchor='application')
 
-            main_infos, prediction = st.columns([2,1])
+            prediction, explain = st.columns([1,2])
 
-            with main_infos:
-                st.write('Coucou!')
-        
             with prediction:
                 st.subheader('Risque de crédit', anchor='credit-risk')
-#                proba_graph = st.container()
-#                pred_text = st.container()
-#                with proba_graph:
+                st.write("Niveau de risque : **{}**".format(
+                    pred_label[pred_final]))
                 st.write('Probabilité de défaut sur le crédit demandé :')
-                pie_sizes =[pred_proba, 1 - pred_proba]
+                pie_sizes =[pred_proba, 1-pred_proba]
                 # Create a pieplot
                 plt.figure(figsize=(6,6))
-                plt.pie(pie_sizes, colors=[color, 'grey'])
+                plt.pie(pie_sizes, colors=[color, 'lightgrey'])
                 # add a circle at the center to transform it in a donut chart
                 my_circle=plt.Circle( (0,0), 0.7, color='white')
                 p=plt.gcf()
                 plt.text(0,0,'{:.1f}%'.format(pred_proba * 100.0),
                             horizontalalignment='center',
                             verticalalignment='center',
-                            fontsize=30)
+                            fontsize=30, fontweight='bold')
                 p.gca().add_artist(my_circle)
                 st.pyplot(p)
-#                with pred_text:
-                st.write("Niveau de risque : **{}**".format(
-                    pred_label[pred_final]))
 
-                st.subheader('Simuler d\'autres conditions d\'emprunt')
-                new_simul = st.button('Simuler')
-        
-        with explain:
-            st.subheader('Facteurs de prédiction', anchor='predict-factors')
-            st_shap(shap_force_plot(details['shap_explainer'],
-                                    details['shap_values'],
-                                    details['Xp'],
-                                    details['feature_names']))
-            for col in ['EXT_SOURCE_1','EXT_SOURCE_2', 'EXT_SOURCE_3',
-                        'AMT_GOODS_PRICE', 'AMT_CREDIT']:
-                if str(api_req[col]['value']) != "nan":
-                    statmnt = '**{}**: {:.0f}%'.format(
-                        col, abs(api_req[col]['gap_pct'] * 100.0))
-                    if api_req[col]['gap_pct'] >= 0.0:
-                        following = 'plus élevé que la médiane des demandes.'
+            with explain:
+                st.subheader('Facteurs de prédiction', anchor='predict-factors')
+                st_shap(shap_force_plot(details['shap_explainer'],
+                                        details['shap_values'],
+                                        details['Xp'],
+                                        details['feature_names']))
+                for col in ['EXT_SOURCE_1','EXT_SOURCE_2', 'EXT_SOURCE_3',
+                            'AMT_GOODS_PRICE', 'AMT_CREDIT']:
+                    if str(api_req[col]['value']) != "nan":
+                        delta = api_req[col]['gap_pct']
+                        pct = abs(delta * 100.0)
+                        following = 'élevé que la médiane des demandes.'
+                        if delta >= 0.0:
+                            sign = 'plus'
+                        else:
+                            sign = 'moins'
+                        s = '**{}**: {:.0f}% {} {}'.format(col, pct,
+                                                           sign, following)
+                        st.write(s)
                     else:
-                        following = 'moins élevé que la médiane des demandes.'
-                    st.write(statmnt + ' ' + following)
-                else:
-                    st.write('**{}**: valeur inconnue.'.format(col))
+                        st.write('**{}**: valeur inconnue.'.format(col))
 
-#        with customer_infos:
-#            st.header('Informations sur l\'emprunteur')
+        main_infos = st.container()
+        with main_infos:
+            st.header('Informations sur le dossier N°{}'.format(api_req['id']))
+            st.subheader('Information sur le demandeur')
+            st.subheader('Information sur le prêt')
+            st.subheader('Simuler d\'autres conditions d\'emprunt')
+            new_simul = st.button('Simuler')
