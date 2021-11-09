40530842a1d8209f9468e4c639f3e91c672b9250
Adrien Dumont <dev@adum.io>
2021-11-09 16:22:35 +0100

    création d'onglets multiples et d'un volet latéral ajout d'infos personnelles sur l'emprunteur ajout d'un onglet pour explorer toute la base client

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -11,7 +11,8 @@ import requests
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 from json.decoder import JSONDecodeError
-from api.api import get_app_details
+from api.api import get_app_details, app_db
+from math import floor
 
 @st.cache
 def request_prediction(model_uri, customer_id):
@@ -47,83 +48,105 @@ def shap_decision_plot(explainer, shap_values, feature_names):
                               shap_values[1],
                               feature_names=feature_names)
 
-# ## DÉBUT DU TABLEAU DE BORD ##
-st.title('PretADepenser - KYC Dashboard', anchor='/')
+def individual():
+    # ## DÉBUT DU TABLEAU DE BORD ##
+#    st.title('PretADepenser - KYC Dashboard', anchor='/')
 
-id_input = st.text_input(label='Entrez un numéro de dossier')
-predict_btn = st.button('Visualiser')
+    id_input = st.text_input(label='Entrez un numéro de dossier')
+    predict_btn = st.button('Visualiser')
 
-if predict_btn:
-    api_req = request_prediction(API_URI, id_input)
-    #st.write(api_req) # debug
-    if api_req['status_code'] == 404:
-        st.write('La demande de prêt n\'est pas présente dans la base.')
-    elif api_req['status_code'] != 200:
-        st.write('Une erreur s\'est produite.')
-    else:
-        details = get_app_details(int(id_input))
-        pred_final = api_req['risk']
-        pred_label = {0: 'Acceptable', 1: 'Critique'}
-        pred_proba = api_req['default_proba']
-        red =  '#FF0D57'
-        blue = '#1E88E5'
-        shap_cm = LinearSegmentedColormap.from_list("", [blue, 'violet', red])
-        color = shap_cm(pred_proba)
+    if predict_btn:
+        req = request_prediction(API_URI, id_input)
+        st.write(req) # debug
+        if req['status_code'] == 404:
+            st.write('La demande de prêt n\'est pas présente dans la base.')
+        elif req['status_code'] != 200:
+            st.write('Une erreur s\'est produite.')
+        else:
+            details = get_app_details(int(id_input))
+            pred_final = req['risk']
+            pred_label = {0: 'Acceptable', 1: 'Critique'}
+            pred_proba = req['default_proba']
+            red =  '#FF0D57'
+            blue = '#1E88E5'
+            shap_cm = LinearSegmentedColormap.from_list("", [blue, 'violet', red])
+            color = shap_cm(pred_proba)
 
-        insights = st.container()
-        explain = st.container()
-        
-        with insights:
-            st.header('Demande de prêt N°{}'.format(api_req['id']),
-                         anchor='application')
+            insights = st.container()
+            explain = st.container()
+            
+            with insights:
+                st.header('Demande de prêt N°{}'.format(req['id']),
+                            anchor='application')
 
-            prediction, explain = st.columns([1,2])
+                prediction, explain = st.columns([1,2])
 
-            with prediction:
-                st.subheader('Risque de crédit', anchor='credit-risk')
-                st.write("Niveau de risque : **{}**".format(
-                    pred_label[pred_final]))
-                st.write('Probabilité de défaut sur le crédit demandé :')
-                pie_sizes =[pred_proba, 1-pred_proba]
-                # Create a pieplot
-                plt.figure(figsize=(6,6))
-                plt.pie(pie_sizes, colors=[color, 'lightgrey'])
-                # add a circle at the center to transform it in a donut chart
-                my_circle=plt.Circle( (0,0), 0.7, color='white')
-                p=plt.gcf()
-                plt.text(0,0,'{:.1f}%'.format(pred_proba * 100.0),
-                            horizontalalignment='center',
-                            verticalalignment='center',
-                            fontsize=30, fontweight='bold')
-                p.gca().add_artist(my_circle)
-                st.pyplot(p)
+                with prediction:
+                    st.subheader('Risque de crédit', anchor='credit-risk')
+                    st.write("Niveau de risque : **{}**".format(
+                        pred_label[pred_final]))
+                    st.write('Probabilité de défaut sur le crédit demandé :')
+                    pie_sizes =[pred_proba, 1-pred_proba]
+                    # Create a pieplot
+                    plt.figure(figsize=(6,6))
+                    plt.pie(pie_sizes, colors=[color, 'lightgrey'])
+                    # add a circle at the center to transform it in a donut chart
+                    my_circle=plt.Circle( (0,0), 0.7, color='white')
+                    p=plt.gcf()
+                    plt.text(0,0,'{:.1f}%'.format(pred_proba * 100.0),
+                                horizontalalignment='center',
+                                verticalalignment='center',
+                                fontsize=30, fontweight='bold')
+                    p.gca().add_artist(my_circle)
+                    st.pyplot(p)
 
-            with explain:
-                st.subheader('Facteurs de prédiction', anchor='predict-factors')
-                st_shap(shap_force_plot(details['shap_explainer'],
-                                        details['shap_values'],
-                                        details['Xp'],
-                                        details['feature_names']))
-                for col in ['EXT_SOURCE_1','EXT_SOURCE_2', 'EXT_SOURCE_3',
-                            'AMT_GOODS_PRICE', 'AMT_CREDIT']:
-                    if str(api_req[col]['value']) != "nan":
-                        delta = api_req[col]['gap_pct']
-                        pct = abs(delta * 100.0)
-                        following = 'élevé que la médiane des demandes.'
-                        if delta >= 0.0:
-                            sign = 'plus'
+                with explain:
+                    st.subheader('Facteurs de prédiction', anchor='predict-factors')
+                    st_shap(shap_force_plot(details['shap_explainer'],
+                                            details['shap_values'],
+                                            details['Xp'],
+                                            details['feature_names']))
+                    for col in ['EXT_SOURCE_1','EXT_SOURCE_2', 'EXT_SOURCE_3',
+                                'AMT_GOODS_PRICE', 'AMT_CREDIT']:
+                        if str(req[col]['value']) != "nan":
+                            delta = req[col]['gap_pct']
+                            pct = abs(delta * 100.0)
+                            following = 'élevé que la médiane des demandes.'
+                            if delta >= 0.0:
+                                sign = 'plus'
+                            else:
+                                sign = 'moins'
+                            s = '**{}**: {:.0f}% {} {}'.format(col, pct,
+                                                            sign, following)
+                            st.write(s)
                         else:
-                            sign = 'moins'
-                        s = '**{}**: {:.0f}% {} {}'.format(col, pct,
-                                                           sign, following)
-                        st.write(s)
-                    else:
-                        st.write('**{}**: valeur inconnue.'.format(col))
+                            st.write('**{}**: valeur inconnue.'.format(col))
 
-        main_infos = st.container()
-        with main_infos:
-            st.header('Informations sur le dossier N°{}'.format(api_req['id']))
-            st.subheader('Information sur le demandeur')
-            st.subheader('Information sur le prêt')
-            st.subheader('Simuler d\'autres conditions d\'emprunt')
-            new_simul = st.button('Simuler')
+            main_infos = st.container()
+            with main_infos:
+                st.header('Informations sur le dossier N°{}'.format(req['id']))
+                st.subheader('Information sur le demandeur')
+                st.write(details['X'])
+                age = floor(- req['DAYS_BIRTH']['value'] / 365)
+                st.write("ÂGE : {}".format(age))
+                st.write("GENRE : {}".format(req['CODE_GENDER']['value']))
+                st.write("PROFESSION : {}".format(req['OCCUPATION_TYPE']['value']))
+                st.write("REVENU ANNUEL : {}".format(req['AMT_INCOME_TOTAL']['value']))
+                st.subheader('Information sur le prêt')
+                st.subheader('Simuler d\'autres conditions d\'emprunt')
+                new_simul = st.button('Simuler')
+
+st.sidebar.title("PretADepenser - KYC Dashboard")
+tab = st.sidebar.radio("Sélectionnez l'onglet souhaité",
+                       ("KYC", "Base client", "Autre"))
+
+if tab == "KYC":
+    individual()
+elif tab == "Base client":
+    st.write("Base client :")
+    display_table = app_db.copy()
+    for col in list(display_table.columns):
+        display_table[col] = display_table[col].astype('category')
+    st.dataframe(display_table)
+elif tab == "Autre":
+    st.write("Autres options (prochainement)")
