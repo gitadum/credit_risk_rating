fa0320c526baf8e48d5264b62547ef1af2bbd49a
Adrien Dumont <dev@adum.io>
2022-01-13 22:35:32 +0100

    taille des fichiers client divisée par 10

--- serialize_dataframes.py
+++ serialize_dataframes.py
@@ -9,16 +9,19 @@ import pandas as pd
 os.mkdir('data_files')
 
 # In[3]:
-app_test = pd.read_csv('02_data/application_test.csv')
+app_test = pd.read_csv('02_data/application_test.csv').loc[::10]
 app_test.to_pickle('data_files/application_test.bz2')
-# %%
-app_train = pd.read_csv('02_data/application_train.csv')
+
+# In[4]:
+app_train = pd.read_csv('02_data/application_train.csv').loc[::10]
 app_train.to_pickle('data_files/application_train.bz2')
-# %%
+
+# In[5]:
 bureau = pd.read_csv('02_data/bureau.csv')
 bureau.to_pickle('data_files/bureau.bz2')
-# %%
-col_desc = pd.read_csv('02_data/HomeCredit_columns_description.csv', index_col=0,
+
+# In[6]:
+col_desc = pd.read_csv('02_data/HomeCredit_columns_description.csv',
+                       index_col=0,
                        encoding='latin9')
 col_desc.to_pickle('data_files/HomeCredit_columns_description.bz2')
-# %%
